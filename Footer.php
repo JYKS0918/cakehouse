97ca9248a8628efebd37dddc77bc65329ca2002d
JYKS0918 <42663383+JYKS0918@users.noreply.github.com>
2018-09-08 20:37:42 +0800

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -1,6 +1,13 @@
 <footer>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
   <hr id="About">
-  <div class="w3-container w3-padding-32 w3-center">
-    <h1 style="text-align:center;">This is just an example</h1>
+  <div class="w3-container w3-padding-32 w3-center w3-sand">
+    <p>Having Trouble Ordering Online?</p>
+    <p>Please do contact us via WhatsApp or WeChat</p>
+    <p>Call us at</p>
+      <h1 style="text-align:center;">Please Follow us On Our Social Network</h1>
+      <div class="w3-xlarge">
+        <a href="www.facebook.com"><i class="fab fa-facebook-square"></i></a>
+      </div>
   </div>
 </footer>
