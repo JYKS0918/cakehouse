04f12272af36d0eb813fe470f409802d3e9b59b1
Alexander-Chai <42663780+Alexander-Chai@users.noreply.github.com>
2018-09-21 09:29:39 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_PORT", "3306");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "blueberry");
+
+define("TB_USER", "user");
+define("TB_CUSTOMER", "customer");
+define("TB_BONUS_POINT", "bonus_point");
+define("TB_STATUS", "status");
+define("TB_PRODUCT", "product");
+define("TB_ITEM_DETAIL", "item_detail");
+define("TB_INVOICE_DETAIL", "invoice_detail");
+?>
